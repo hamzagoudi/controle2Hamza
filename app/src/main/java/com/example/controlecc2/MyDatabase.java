93721dc6e4b7ff3c703318b6232b5e9138f314fa
GOUDI <goudihamza@gmail.com>
2022-06-03 17:09:03 +0200

    Initial commit

---
+++ app/src/main/java/com/example/controlecc2/MyDatabase.java
@@ -0,0 +1,73 @@
+package com.example.controlecc2;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+public class MyDatabase extends SQLiteOpenHelper {
+
+    public static String db_name = "societes.db";
+    public static String table_name = "Societe";
+    public static String col1 = "ID";
+    public static String col2 = "Raison_Sociale";
+    public static String col3 = "Secteur_activite";
+    public static String col4 = "nb_employe";
+
+    public MyDatabase(Context context) {
+        super(context, db_name, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String sql = String.format("create table %s(%s integer autoincrement primary key,%s text,%s text,%s integer)", table_name, col1, col2, col3, col4);
+        sqLiteDatabase.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String sql = "drop table " + table_name;
+        sqLiteDatabase.execSQL(sql);
+        onCreate(sqLiteDatabase);
+    }
+
+    public static long AddSociete(SQLiteDatabase db, Societe e) {
+        ContentValues cv = new ContentValues();
+        cv.put(col2, e.getNom());
+        cv.put(col3, e.getSecteur());
+        cv.put(col4, e.getNbEmploye());
+        return db.insert(table_name, null, cv);
+    }
+
+    public static long UpdateSociete(SQLiteDatabase db, Societe e) {
+        ContentValues cv = new ContentValues();
+        cv.put(col2, e.getNom());
+        cv.put(col3, e.getSecteur());
+        cv.put(col4, e.getNbEmploye());
+        return db.update(table_name, cv, "ID=" + e.getId(), null);
+    }
+
+    public static long DeleteSociete(SQLiteDatabase db, int id) {
+        return db.delete(table_name, "ID=" + id, null);
+    }
+
+    public static ArrayList<Societe> getAllSociete(SQLiteDatabase db) {
+        Cursor c = db.rawQuery("select * from " + table_name, null);
+        ArrayList<Societe> ss = new ArrayList<>();
+        while (c.moveToNext())
+            ss.add(new Societe(c.getInt(0), c.getString(1), c.getString(2), c.getInt(3)));
+        return ss;
+    }
+
+    public static Societe getOneSociete(SQLiteDatabase db, int id) {
+        Cursor c = db.rawQuery("select * from " + table_name + " where ID=" + id, null);
+        Societe s = null;
+        if (c.moveToNext())
+            s = new Societe(c.getInt(0), c.getString(1), c.getString(2), c.getInt(3));
+        return s;
+    }
+}
+
---
+++ app/src/main/java/com/example/controlecc2/Societe.java
@@ -0,0 +1,61 @@
+package com.example.controlecc2;
+
+import java.io.Serializable;
+
+public class Societe implements Serializable {
+    private int id;
+    private String Nom;
+    private String Secteur;
+    private int NbEmploye;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return Nom;
+    }
+
+    public void setNom(String nom) {
+        this.Nom = nom;
+    }
+
+    public String getSecteur() {
+        return Secteur;
+    }
+
+    public void setSecteur(String secteur) {
+        this.Secteur = secteur;
+    }
+
+    public int getNbEmploye() {
+        return NbEmploye;
+    }
+
+    public void setNbEmploye(int nbEmploye) {
+        this.NbEmploye = nbEmploye;
+    }
+
+    public Societe() {
+    }
+
+    public Societe(int id, String nom, String secteur, int nbEmploye) {
+        this.id = id;
+        this.Nom = nom;
+        this.Secteur = secteur;
+        this.NbEmploye = nbEmploye;
+    }
+
+    public Societe(String nom, String secteur, int nbEmploye) {
+        this.Nom = nom;
+        this.Secteur = secteur;
+        this.NbEmploye = nbEmploye;
+    }
+}
+
+
+
