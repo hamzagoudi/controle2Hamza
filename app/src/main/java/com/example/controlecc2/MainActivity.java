c736287a6e17802fe2d1d797f033b6c2c28b2282
GOUDI <goudihamza@gmail.com>
2022-06-03 17:32:00 +0200

    exo6 exo7

--- app/src/main/java/com/example/controlecc2/MainActivity.java
+++ app/src/main/java/com/example/controlecc2/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.controlecc2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void open(View view) {
+
+        Intent i=null;
+        switch(view.getId()){
+            case R.id.btn_aj: i=new Intent(MainActivity.this,ajouter.class);break;
+            case R.id.btn_ed: i=new Intent(MainActivity.this,editer.class);break;
+            case R.id.btn_li: i=new Intent(MainActivity.this,lister.class);break;
+        }
+        startActivity(i);
     }
 }
 
